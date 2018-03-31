d224ac5c9f7b300e3c483903322d07c38ea0c0a2
santhosh <santhoshkumara1996@gmail.com>
2018-03-31 22:09:13 +0530

    some more modification

--- script/doc reader/test.py
+++ script/doc reader/test.py
@@ -35,12 +35,12 @@ class LstmPreprocessLayer(torch.nn.Module):
 		# print(inputs.unsqueeze(1).shape)
 		# exit()
 		
-		output = self.lstm(inputs.unsqueeze(1))[0]
+		output,hidden = self.lstm(inputs.unsqueeze(1),hidden)
 		
-		return output
+		return output,hidden
 
 	def initHidden(self):
-		return(Variable(torch.zeros(1,self.hidden_size)))
+		return Variable(torch.zeros(1,1,self.hidden_size)),Variable(torch.zeros(1,1,self.hidden_size))
 
 
 class LstmAttentionLayer(torch.nn.Module):
@@ -59,21 +59,41 @@ class LstmAttentionLayer(torch.nn.Module):
 		self.softmax = torch.nn.Softmax(dim =1)
 	def forward(self,x,hidden,Hq):
 
-		temp = self.tanh(self.Wq(Hq) + ( self.lstm(x.view(1,-1).unsqueeze(1))[0].squeeze(1) ).repeat(self.number_Q_tok,1))
-		# output = self.lstm(x.view(1,-1).unsqueeze(1))[0]
-		# output = output.squeeze(1)
-		# output = output.repeat(self.number_Q_tok,1)
+		# temp = self.tanh(self.Wq(Hq) + ( self.lstm(x.view(1,-1).unsqueeze(1),hidden)[0].squeeze(1) ).repeat(self.number_Q_tok,1))
+		temp = self.tanh(self.Wq(Hq))
+		output,hidden = self.lstm(x.view(1,-1).unsqueeze(1),hidden)
+		output = output.squeeze(1)
+		output = output.repeat(self.number_Q_tok,1)
+		output = output+temp
 		# print(temp.shape)
 		# alpha = self.softmax(self.w(temp))
 		# exit()
-		return temp
+		return output,hidden
 
 	def initHidden(self):
-		return(Variable(torch.zeros(1,self.hidden_size)))
-p = LstmPreprocessLayer(100,150,2)
+		return Variable(torch.zeros(1,1,self.hidden_size)),Variable(torch.zeros(1,1,self.hidden_size))
 
-Hp = p(datalc[0]['0'],p.initHidden())
-Hq = p(datalq[0]['Q'],p.initHidden())
+class MatchLstmLayer(torch.nn.Module):
+	def __init__(self,input_size,hidden_size,number_layer):
+		super(MatchLstmLayer,self).__init__()
+
+		self.input_size = input_size
+		self.hidden_size = hidden_size
+		self.number_layer = number_layer
+
+		self.lstm = torch.nn.LSTM(self.input_size,self.hidden_size,self.number_layer)
+		
+	def forward(self,x,hidden):
+		output,hidden = self.lstm(x.view(1,-1).unsqueeze(1),hidden)
+		return output,hidden
+
+	def initHidden(self):
+		return Variable(torch.zeros(1,1,self.hidden_size)),Variable(torch.zeros(1,1,self.hidden_size))
+
+p = LstmPreprocessLayer(100,150,1)
+
+Hp,hidden = p(datalc[0]['0'],p.initHidden())
+Hq,hidden = p(datalq[0]['Q'],p.initHidden())
 # print(Hp[0].squeeze(1).shape,Hq.squeeze(1).shape)
 # print(Hp,Hq)
 Hp = Hp.squeeze(1)
@@ -83,11 +103,11 @@ a = LstmAttentionLayer(150,150,1,len(Hq))
 hidden = a.initHidden()
 h = Variable()
 for i in Hp:
-	hidden = a(i,hidden,Hq)
+	output,hidden = a(i,hidden,Hq)
 	try:
-		h = torch.cat(h,hidden)
+		h = torch.cat(h,output)
 	except:
-		h =  hidden
+		h =  output
 # print(h.shape)
 alpha = a.softmax(a.w(h))
 x = torch.mm(torch.t(Hq),alpha)
@@ -95,5 +115,15 @@ x = torch.t(x.repeat(1,len(Hp)))
 
 
 Z = torch.cat((Hp,x),1)
+print(Z.shape)	#141 X 300
 
-print(Z.shape)
+m = MatchLstmLayer(300,150,1)
+hidden = m.initHidden()
+for i in Z:
+	output,hidden = m(i,hidden)
+	try:
+		hr = torch.cat(h,output)
+	except:
+		hr =  output
+print("output Tensor",hr.shape)
+
